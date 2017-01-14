3104cd12ede0c2009a406a96b8dea5b197b1dd9c
Krzysztof Swieczkowski <krzyszt.swieczkowski@gmail.com>
2017-01-14 23:34:20 +0100

    wypisywanie wynikow

--- esc/src/main/java/pl/pg/gda/eti/kio/esc/bayes/BayesClassifier.java
+++ esc/src/main/java/pl/pg/gda/eti/kio/esc/bayes/BayesClassifier.java
@@ -9,9 +9,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author Wojciech Stanisławski, Krzysztof Świeczkowski
@@ -21,6 +25,8 @@ public class BayesClassifier {
 	
 	public static void main(String[] args) throws IOException {
 	//public void classify(/* coś tu będzie */) {
+		BayesClassificationForArticles classification = new BayesClassificationForArticles();
+		
 		WordDictionaryMerger merger = new WordDictionaryMerger();
 		merger.mergeFiles("simple/temp-po_slowach-feature_dict-simple-20120104", "en/en-po_slowach-feature_dict-en-20111201", 1);
 
@@ -35,9 +41,7 @@ public class BayesClassifier {
 		
 		BayesConditionalProbability bayesConditionalProbability = new BayesConditionalProbability();
 		Map<String, BayesConditionalProbability.ConditionalProbabilityForClass> conditionalProbability = bayesConditionalProbability.countConditionalProbability(stringWordsInCategoryMap, wordsInDictionary);
-		
-		//TODO: prawdopodobienstwo przynaleznosci dokumentu do kazdej z kategorii
-		
+			
 		//artykuly do przypasowania	
 		List<WordFeature> mergedDictionary = merger.getChunks()[0];
 		
@@ -65,18 +69,50 @@ public class BayesClassifier {
 					}
 			    }
 			}
+			Map<Double, String> bayesClassificationForArticle = new TreeMap<Double, String>();
 			//TODO: create return object
 			//foreach class check if wordfeature exists and count probability
 			for (Map.Entry<String, BayesConditionalProbability.ConditionalProbabilityForClass> conditionalProbabilityForClass : conditionalProbability.entrySet()) {
-				Double bayesClassification = 1.0; //TODO: podmienic na prawdopodobienstwo przynaleznosci dokumentu do kazdej z kategorii
+				Double bayesClassification = (double) categoryStatistics.articlesInCategoriesCount.get(conditionalProbabilityForClass.getKey()) / categoryStatistics.articlesCount; //TODO: podmienic na prawdopodobienstwo przynaleznosci dokumentu do kazdej z kategorii
+				//liczba kategorii w klasie przez liczbe wszystkich art 
 				for (WordFeature wordFeatureInArticle : wordFeaturesInArticle) {
 					if(conditionalProbabilityForClass.getValue().conditionalProbabilityForWordInClass.containsKey(wordFeatureInArticle)) {
 						bayesClassification *= conditionalProbabilityForClass.getValue().conditionalProbabilityForWordInClass.get(wordFeatureInArticle);
 					}
 				}
-				System.out.println(bayesClassification);
+				bayesClassificationForArticle.put(bayesClassification, conditionalProbabilityForClass.getKey());			
 			}
+			classification.put(articleId, bayesClassificationForArticle);
 		}
 		stream.close();
+
+		classification.getClassificationResults(2);
+	}
+	
+	public static class BayesClassificationForArticles {
+		Map<String, Map<Double, String>> classification;
+		
+		public BayesClassificationForArticles() {
+			classification = new HashMap<String, Map<Double, String>>();
+		}
+				
+		public void getClassificationResults(Integer noOfBestClasses) {
+			for (Map.Entry<String, Map<Double, String>> classificationForArticle : classification.entrySet()) {
+				String result = classificationForArticle.getKey() + "\t";
+				int counter = noOfBestClasses;
+				for (Map.Entry<Double, String> bayesClassification : classificationForArticle.getValue().entrySet()) {
+					if(counter == 0) {
+						break;
+					}
+					result += "(" + bayesClassification.getValue().toString() + "," + bayesClassification.getKey() + ")\t";
+					counter--;
+				}
+				System.out.println(result);
+			}
+		}
+		
+		public void put(String articleId, Map<Double, String> bayesClassificationForArticle) {
+			classification.put(articleId, bayesClassificationForArticle);
+		}
 	}
 }
