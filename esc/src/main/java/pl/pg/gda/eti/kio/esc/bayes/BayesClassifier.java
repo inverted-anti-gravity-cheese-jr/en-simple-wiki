489ba8246f0b7183a97ad0210c0c0b37aadd8fac
Krzysztof Swieczkowski <krzyszt.swieczkowski@gmail.com>
2017-01-14 21:03:46 +0100

    prawie skonczony Bayes, brakuje prawdopodobienstwa przynaleznosci dokumentu do kategorii

--- esc/src/main/java/pl/pg/gda/eti/kio/esc/bayes/BayesClassifier.java
+++ esc/src/main/java/pl/pg/gda/eti/kio/esc/bayes/BayesClassifier.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -31,20 +32,16 @@ public class BayesClassifier {
 
 		BayesWordInCategoryCounter bayesWordInCategoryCounter = new BayesWordInCategoryCounter();
 		Map<String, BayesWordInCategoryCounter.WordsInCategory> stringWordsInCategoryMap = bayesWordInCategoryCounter.countWordsInCategories("simple/temp-po_slowach-categories-simple-20120104", "simple/temp-po_slowach-lista-simple-20120104", merger.getChunks()[0]);
-			
+		
 		BayesConditionalProbability bayesConditionalProbability = new BayesConditionalProbability();
 		Map<String, BayesConditionalProbability.ConditionalProbabilityForClass> conditionalProbability = bayesConditionalProbability.countConditionalProbability(stringWordsInCategoryMap, wordsInDictionary);
 		
+		//TODO: prawdopodobienstwo przynaleznosci dokumentu do kazdej z kategorii
+		
 		//artykuly do przypasowania	
-		//WordDictionaryMerger wordDictionaryMerger = new WordDictionaryMerger();
-		//wordDictionaryMerger.mergeFiles("simple/temp-po_slowach-feature_dict-simple-20120104", "en/en-po_slowach-feature_dict-en-20111201", 1);
-		//List<WordFeature> mergedDictionary = wordDictionaryMerger.getChunks()[0];
-	}
-}
-
-
-/*
-//Ładowanie pliku		
+		List<WordFeature> mergedDictionary = merger.getChunks()[0];
+		
+		//Ładowanie pliku		
 		File file = new File("en/en-po_slowach-lista-en-20111201");
 		BufferedReader stream = new BufferedReader(new FileReader(file));
 		String line;
@@ -52,10 +49,14 @@ public class BayesClassifier {
 		while ((line = stream.readLine()) != null) {
 			//geting article id
 			String[] elements = line.split("#");
+			if(elements.length < 2) {
+				continue;
+			}
 			String articleId = elements[0];
 			//getting words in article
 			List<WordFeature> wordFeaturesInArticle = new ArrayList<WordFeature>();
 			String[] words = elements[1].split(" ");
+			//foreach word get wordFeatures
 			for(int  i=0; i < words.length; i++) {
 				String wordId = words[i].split("-")[0];
 				for (WordFeature feature : mergedDictionary) {
@@ -64,6 +65,19 @@ public class BayesClassifier {
 					}
 			    }
 			}
+			//TODO: create return object
+			//foreach class check if wordfeature exists and count probability
+			for (Map.Entry<String, BayesConditionalProbability.ConditionalProbabilityForClass> conditionalProbabilityForClass : conditionalProbability.entrySet()) {
+				Double bayesClassification = 1.0; //TODO: podmienic na prawdopodobienstwo przynaleznosci dokumentu do kazdej z kategorii
+				for (WordFeature wordFeatureInArticle : wordFeaturesInArticle) {
+					if(conditionalProbabilityForClass.getValue().conditionalProbabilityForWordInClass.containsKey(wordFeatureInArticle)) {
+						bayesClassification *= conditionalProbabilityForClass.getValue().conditionalProbabilityForWordInClass.get(wordFeatureInArticle);
+					}
+				}
+				System.out.println(bayesClassification);
+			}
+			return;
 		}
 		stream.close();
-*/
+	}
+}
